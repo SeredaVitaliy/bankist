d15ac7e0e529276f155358dd3768063602a1f454
Sereda Vitaliy <sereda.v484@gmail.com>
2025-09-16 11:50:46 +0200

    Добавление фукционала удаление массива с пользователем

--- script.js
+++ script.js
@@ -422,3 +422,25 @@ btnTransfer.addEventListener('click', function (e) {
     updateUI(currentAccount);
   }
 });
+
+//закрытие(удаление) учетной записи
+btnClose.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  if (
+    inputCloseUsername.value === currentAccount.username &&
+    Number(inputClosePin.value) === currentAccount.pin
+  ) {
+    const index = accounts.findIndex(
+      acc => acc.username === currentAccount.username
+    );
+    console.log(index);
+
+    //удаление учетной записи
+    accounts.splice(index, 1);
+
+    //скрытие UI
+    containerApp.style.opacity = 0;
+  }
+  inputCloseUsername.value = inputClosePin.value = '';
+});
